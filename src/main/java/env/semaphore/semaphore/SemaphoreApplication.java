410f529661a644d404436d3519c5ced285dc80ec
yunkai <yunkai@yunkaideMacBook-Pro.local>
2018-04-02 20:01:38 +0800

    项目构建

---
+++ src/main/java/env/semaphore/semaphore/SemaphoreApplication.java
@@ -0,0 +1,12 @@
+package env.semaphore.semaphore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemaphoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemaphoreApplication.class, args);
+	}
+}
---
+++ src/test/java/env/semaphore/semaphore/SemaphoreApplicationTests.java
@@ -0,0 +1,16 @@
+package env.semaphore.semaphore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SemaphoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
